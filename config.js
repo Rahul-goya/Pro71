7d56c45051c0857bf3ff0fd330528295a18e29b3
Rahul-goya <84616345+Rahul-goya@users.noreply.github.com>
2022-06-26 20:59:05 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -2,8 +2,15 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 //Paste your firebaseConfig here
-
-//
+const firebaseConfig = {
+    apiKey: "AIzaSyBeba3hab7FunNJ3L0EnUB2RIDDX-esEIs",
+    authDomain: "complanit-fourm.firebaseapp.com",
+    projectId: "complanit-fourm",
+    storageBucket: "complanit-fourm.appspot.com",
+    messagingSenderId: "806948275354",
+    appId: "1:806948275354:web:85d58f6b0602fcb958cb23"
+  };
+  
 firebase.initializeApp(firebaseConfig);
 
 export default firebase.firestore();
